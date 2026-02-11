296f7d0534d3256c5283f1540a43cc0391ab8afd
spotdemo4 <me@trev.xyz>
2026-02-11 06:38:58 -0500

    fix: destroy request after getting response

--- src/init.ts
+++ src/init.ts
@@ -19,8 +19,7 @@ async function main() {
 	core.info(`Checking connectivity to ${server}`);
 	const client = new httpc.HttpClient();
 	const resp = await client.head(server);
-	// client.dispose();
-	// resp.message.destroy();
+	resp.message.destroy();
 	if (!resp.message.statusCode || resp.message.statusCode >= 400) {
 		throw new Error(
 			`Failed to connect to ${server}: ${resp.message.statusCode} ${resp.message.statusMessage}`,
